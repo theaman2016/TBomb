f3942948796b2c17a8e7701c475a5fb563b1fa57
theaman2016 <58293221+theaman2016@users.noreply.github.com>
2020-04-10 09:03:31 +0530

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -534,11 +534,11 @@ try:
 except Exception:
     type = 0
 if type == 1:
-    nm = int(input("Enter Number of Calls To Send(Maximum 15): "))
-    if nm > 15:
+    nm = int(input("Enter Number of Calls To Send(Maximum 500): "))
+    if nm > 500:
         print("\t\tYou Have Entered " + str(nm) +
-              ".\n\tNormalizing Value To 15")
-        nm = 15
+              ".\n\tNormalizing Value To 500")
+        nm = 500
     dl = float(input("Enter Delay time (in seconds) [Recommended 10 sec ] : "))
 elif type == 0:
     if cc == "91":
@@ -551,7 +551,7 @@ elif type == 0:
             input("Enter Delay time (in seconds) [Recommended 10 sec ] : "))
 maxlim = 0
 if cc == "91":
-    maxlim = 500
+    maxlim = 6000
 else:
     maxlim = 100
 if nm > maxlim:
